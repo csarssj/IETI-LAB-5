98d2bdb1820f9039a684d2066266b9af73d6aa33
csarssj <cesar.gonzalez-c@mail.escuelaing.edu.co>
2021-03-04 16:02:08 -0500

    :sparkles: firebase

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# IETI-LAB-5
+# IETI-LAB-4
+# Código de Honor
+
+Debes seguir el Código de honor del ingeniero de sistemas para defender el estándar de integridad académica de la ECI:
+
+* Tus respuestas a tareas, cuestionarios y exámenes deben ser tu propio trabajo (excepto para las tareas que permiten explícitamente la colaboración).
+
+* No puedes compartir tus soluciones de tareas, cuestionarios o exámenes con otra persona a menos que el instructor lo permita explícitamente. Esto incluye cualquier cosa escrita por ti, como también cualquier solución oficial proporcionada por el docente o el monitor del curso.
+
+* No puedes participar en otras actividades que mejorarán de manera deshonesta tus resultados o que mejorarán de manera deshonesta o dañarán los resultados de otras personas.
+
+# Authors
+## Credenciales 
+user; cesego98@gmail.com
+contraseña: hola123
+[César González](https://github.com/csarssj) 
+
+_Fecha : 5 de febrero del 2021_ 
+# Despliegue
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://task-planner-app4.herokuapp.com/
+)
---
+++ src/App.js
@@ -0,0 +1,52 @@
+  
+import React, { Component } from "react";
+import "./App.css";
+import { Login } from "./components/Login";
+import { Home } from "./components/Home";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import NewTask from "./components/NewTask";
+import { UserProfile } from "./components/UserProfile";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+    };
+    this.handleIsLoggedIn = this.handleIsLoggedIn.bind(this);
+  }
+  handleIsLoggedIn() {
+    this.setState({
+      isLoggedIn: true
+    });
+  }
+
+  render() {
+    var redirect;
+    if(this.state.isLoggedIn === false && localStorage.getItem("email") === null){
+      redirect = <Redirect to = {"/"}/>
+    }else{
+      redirect = <Redirect to = {"/home"}/>
+    }
+    const LoginView = () => <Login login={this.handleIsLoggedIn} />;
+    const TodoAppView = () => <div>
+                                <Home />
+                              </div>;
+    const newTask = () => <NewTask />;
+    const userProfile = () => <UserProfile />;
+    return (
+      <Router>
+      <div className="App">
+       
+          <div>
+          <Route exact path="/" component={LoginView}/>
+          <Route path="/home" component={TodoAppView} />
+          <Route path="/new-task" component={newTask} />
+          <Route path="/user-profile" component={userProfile} />
+          </div>
+      </div>
+    </Router>
+    );
+  }
+}
+
+export default App;
